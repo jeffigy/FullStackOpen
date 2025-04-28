805de252d7405b79173263d2437baedb7aa1bcd0
Jeffy Evangelista <ejeffydev@gmail.com>
2025-04-29 01:28:49 +0800

    skipped part-6

--- part-6/notes/src/components/Note.tsx
+++
@@ -1,7 +0,0 @@
-import { NoteType } from "../types/note";
-
-const Note = ({ note }: { note: NoteType }) => {
-  return <li>{note.content}</li>;
-};
-
-export default Note;
--- part-6/notes/src/types/note.ts
+++
@@ -1,5 +0,0 @@
-export type NoteType = {
-  id: number;
-  content: string;
-  important: boolean;
-};
